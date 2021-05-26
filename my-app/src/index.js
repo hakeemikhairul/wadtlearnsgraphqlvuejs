06691d8819dabe4c58dcc4defba6999de8ff7e76
Ulfi Shaffarani <19ftt1160@student.pb.edu.bn>
2021-05-26 20:16:57 +0800

    server uri

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,7 +5,7 @@ import VueApollo from "vue-apollo";
 
 const apolloProvider = new VueApollo({
   defaultClient: new ApolloClient({
-    uri: '/server'
+    uri: 'http://localhost:4001'
   })
 });
 
@@ -14,6 +14,6 @@ Vue.use(VueApollo);
 
 new Vue({
   el: '#app',
-  apolloProvider, 
+  apolloProvider,
   render: h => h(App)
 })
